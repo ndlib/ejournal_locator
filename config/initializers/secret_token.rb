2e294812c2e34b7a268be7ee04329dc8541ff5aa
Jaron Kennel <jaronkk@gmail.com>
2012-04-11 14:15:24 -0400

    Starting new Rails 3.2.3 project for Ejournal Locator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EjournalLocator::Application.config.secret_token = '1d9c6b2143f42dfd971dba6d8729dbf5a7d35d63870d1ae6fb5815b844cc760eda5617d7a3fe1a2eeb081e981c1feaa55b165faa9abee5ef6cc22d499832a459'
