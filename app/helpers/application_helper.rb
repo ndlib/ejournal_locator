93c2e6a96590fedef90493e9294e3a25fe0c5a8c
Jaron Kennel <jaronkk@gmail.com>
2012-04-17 17:33:51 -0400

    Implementing library layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  # Includes the relevant library SSI file from https://www.library.nd.edu/ssi/<filename>.shtml
+  def include_ssi(filename)
+    render :partial => "/layouts/include_ssi", :locals => {:filename => filename}
+  end
+
+  def read_ssi_file(filename)
+    require 'open-uri'
+    ssi_url = "http://www.library.nd.edu/ssi/#{filename}.shtml"
+    f = open(ssi_url, "User-Agent" => "Ruby/#{RUBY_VERSION}")
+    contents = f.read
+    contents = contents.gsub(/(href|src)="\//,"\\1=\"https://www.library.nd.edu/")
+    contents
+  end
 end
