cf10b82a9cb4ef63252b1b7d145bdfc49ea2e097
Jaron Kennel <jaronkk@gmail.com>
2012-07-02 12:59:42 -0400

    Minor layout updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  # Includes the relevant library SSI file from https://www.library.nd.edu/ssi/<filename>.shtml
+  # Includes the relevant library SSI file from http://www.library.nd.edu/ssi/<filename>.shtml
   def include_ssi(filename)
     render :partial => "/layouts/include_ssi", :locals => {:filename => filename}
   end
@@ -10,7 +10,15 @@ module ApplicationHelper
     ssi_url = "http://www.library.nd.edu/ssi/#{filename}.shtml"
     f = open(ssi_url, "User-Agent" => "Ruby/#{RUBY_VERSION}")
     contents = f.read
+    if filename == "js"
+      contents = clean_ssi_js(contents)
+    end
     contents = contents.gsub(/(href|src)="\//,"\\1=\"https://www.library.nd.edu/")
     contents
+  end
+
+  # Since we're in the context of a Rails application with its own javascript assets, we want to remove any library site javascripts we don't need
+  def clean_ssi_js(contents)
+    contents.gsub(/^.*(jquery|simplegallery).*$/,"")
   end
 end
