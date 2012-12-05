5b34c0116bcb7f8166511ecf73947d8e77ba9fef
Jaron Kennel <jaronkk@gmail.com>
2012-12-05 13:09:18 -0500

    Add the a-z list links at the top of the search form

--- app/helpers/ejournal_helper.rb
+++ app/helpers/ejournal_helper.rb
@@ -3,4 +3,8 @@ module EjournalHelper
     object_id = document.id.gsub(/^journal-/,'')
     "http://findtext.library.nd.edu:8889/ndu_local?url_ver=Z39.88-2004&ctx_ver=Z39.88-2004&ctx_enc=info:ofi/enc:UTF-8&rfr_id=info:sid/ND_ejl_loc&url_ctx_fmt=info:ofi/fmt:kev:mtx:ctx&svc_val_fmt=info:ofi/fmt:kev:mtx:sch_svc&sfx.ignore_date_threshold=1&rft.object_id=#{object_id}"
   end
+
+  def journal_a_z_links
+    render :partial => "/catalog/a_z_links"
+  end
 end
