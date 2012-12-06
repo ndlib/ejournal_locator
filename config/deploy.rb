68d5ea87f989bad198bef17a117e251dfaddb98b
Jaron Kennel <jaronkk@gmail.com>
2012-12-06 17:17:43 -0500

    Added a deploy task to reload the Solr core

--- config/deploy.rb
+++ config/deploy.rb
@@ -120,8 +120,8 @@ namespace :deploy do
     solr_config = YAML.load_file("#{release_path}/config/solr.yml")[rails_env.to_s]
     core_url = solr_config["url"]
     core_regex = /[^\/]+$/
-    core_name = original_url.match(core_regex)[0]
-    base_solr_url = original_url.gsub(core_regex,'')
+    core_name = core_url.match(core_regex)[0]
+    base_solr_url = core_url.gsub(core_regex,'')
     reload_url = base_solr_url + "admin/cores?action=RELOAD&core=" + core_name
     puts "Reloading solr core: #{reload_url}"
     run "curl -I -A \"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)\" #{reload_url}"
