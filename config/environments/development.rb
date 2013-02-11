45bb1ba59f695133ff8a45a26e29c714ea776eee
Jaron Kennel <jaronkk@gmail.com>
2013-02-11 15:15:53 -0500

    Development.rb cleanup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ EjournalLocator::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -34,6 +34,4 @@ EjournalLocator::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
-  config.serve_static_assets = false
 end
