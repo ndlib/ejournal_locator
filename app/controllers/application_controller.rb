984bb3e40aaebd2bde7b0169f77d6208803fca88
Jaron Kennel <jaronkk@gmail.com>
2013-04-29 14:06:29 -0400

    Style tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   define_callbacks :logging_in_user
-  # Adds a few additional behaviors into the application controller 
+  # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
   layout 'application'
-  # Please be sure to impelement current_user and user_session. Blacklight depends on 
-  # these methods in order to perform user specific actions. 
+  # Please be sure to impelement current_user and user_session. Blacklight depends on
+  # these methods in order to perform user specific actions.
 
   protect_from_forgery
 end
